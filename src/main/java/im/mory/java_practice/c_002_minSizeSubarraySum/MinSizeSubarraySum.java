f8dffd51fe7dd6d452328e07bb67ad65cea33975
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-06-09 21:23:34 +0100

    solved

--- src/main/java/im/mory/java_practice/c_002_minSizeSubarraySum/MinSizeSubarraySum.java
+++ src/main/java/im/mory/java_practice/c_002_minSizeSubarraySum/MinSizeSubarraySum.java
@@ -1,4 +1,22 @@
 package im.mory.java_practice.c_002_minSizeSubarraySum;
 
 public class MinSizeSubarraySum {
+    public int Solution(int target, int[] nums) {
+        //7
+        //min=2
+        // 2 3 1 2 |4 3|
+
+        var left = 0;
+        var sum = 0;
+        int minLength = Integer.MAX_VALUE;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum >= target && left <= right) {
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
 }
