e4ddb1874dd3c0c1262ae591c18600f18b5d8057
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-06-10 07:51:01 +0100

    add test cases

---
+++ src/main/java/im/mory/java_practice/c004_minRemoveValidParentheses/MinRemoveValidParentheses.java
@@ -0,0 +1,7 @@
+package im.mory.java_practice.c004_minRemoveValidParentheses;
+
+public class MinRemoveValidParentheses {
+    public String Solution(String s) {
+        return "";
+    }
+}
---
+++ src/test/java/im/mory/java_practice/c004_minRemoveValidParentheses/MinRemoveValidParenthesesTest.java
@@ -0,0 +1,18 @@
+package im.mory.java_practice.c004_minRemoveValidParentheses;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MinRemoveValidParenthesesTest {
+    private MinRemoveValidParentheses m;
+
+    @BeforeEach
+    void setUp() {
+        m = new MinRemoveValidParentheses();
+    }
+
+    @Test
+    void testSolution() {
+
+    }
+}
