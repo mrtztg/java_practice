9a69757744958825cb465a53b5aaf64d3ba170f1
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-06-10 07:45:54 +0100

    solve houseRobber

--- src/main/java/im/mory/java_practice/c003_houseRobber/HouseRobber.java
+++ src/main/java/im/mory/java_practice/c003_houseRobber/HouseRobber.java
@@ -1,7 +1,18 @@
 package im.mory.java_practice.c003_houseRobber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HouseRobber {
     public int Solution(int[] nums) {
-
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        List<Integer> vals = new ArrayList<>();
+        vals.add(nums[0]);
+        vals.add(Math.max(nums[1], vals.get(0)));
+        for (int i = 2; i < nums.length; i++) {
+            vals.add(Math.max(nums[i] + vals.get(i-2), vals.get(i-1)));
+        }
+        return vals.get(nums.length - 1);
     }
 }
--- src/test/java/im/mory/java_practice/c003_houseRobber/HouseRobberTest.java
+++ src/test/java/im/mory/java_practice/c003_houseRobber/HouseRobberTest.java
@@ -19,8 +19,11 @@ public class HouseRobberTest {
                 () -> assertEquals(4, h.Solution(new int[]{1, 2, 3, 1})),
                 () -> assertEquals(12, h.Solution(new int[]{2, 7, 9, 3, 1})),
                 () -> assertEquals(2, h.Solution(new int[]{1, 2})),
+                () -> assertEquals(2, h.Solution(new int[]{2, 1})),
                 () -> assertEquals(100, h.Solution(new int[]{100})),
-                () -> assertEquals(31, h.Solution(new int[]{1, 4, 10, 3, 4, 20}))
+                () -> assertEquals(31, h.Solution(new int[]{1, 4, 10, 3, 4, 20})),
+                () -> assertEquals(15, h.Solution(new int[]{2, 5, 1, 2, 10}))
+//                () -> assertEquals(15, h.Solution(new int[]{2, 5, 1, 2, 10, 6, 3, 2}))
         );
     }
 }
