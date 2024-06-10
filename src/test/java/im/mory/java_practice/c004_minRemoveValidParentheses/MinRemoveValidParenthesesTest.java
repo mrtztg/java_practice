a2cbb85a158d8ba8bf2313f7d9d0bc518909ee27
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-06-10 07:55:01 +0100

    add test cases

--- src/test/java/im/mory/java_practice/c004_minRemoveValidParentheses/MinRemoveValidParenthesesTest.java
+++ src/test/java/im/mory/java_practice/c004_minRemoveValidParentheses/MinRemoveValidParenthesesTest.java
@@ -3,6 +3,8 @@ package im.mory.java_practice.c004_minRemoveValidParentheses;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class MinRemoveValidParenthesesTest {
     private MinRemoveValidParentheses m;
 
@@ -13,6 +15,10 @@ class MinRemoveValidParenthesesTest {
 
     @Test
     void testSolution() {
-
+        assertAll(
+                () -> assertEquals("lee(t(c)o)de", "lee(t(c)o)de)"),
+                () -> assertEquals("ab(c)d", "a)b(c)d"),
+                () -> assertEquals("", "))((")
+        );
     }
 }
