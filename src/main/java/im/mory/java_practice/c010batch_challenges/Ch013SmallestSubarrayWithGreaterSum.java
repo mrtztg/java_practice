3861e00b8356e937961c63badd490a2da9076864
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-06-18 21:20:40 +0100

    Add initialization for 'left' and 'right' variables

--- src/main/java/im/mory/java_practice/c010batch_challenges/Ch013SmallestSubarrayWithGreaterSum.java
+++ src/main/java/im/mory/java_practice/c010batch_challenges/Ch013SmallestSubarrayWithGreaterSum.java
@@ -2,6 +2,6 @@ package im.mory.java_practice.c010batch_challenges;
 
 public class Ch013SmallestSubarrayWithGreaterSum {
     int findMinSubArray(int S, int[] arr) {
-
+        int left = 0, right = 0;
     }
 }
