a513d3b6b49031131f5e47a860f73e6926802517
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-06-18 20:56:17 +0100

    solve maxSumSubarrayK

--- src/main/java/im/mory/java_practice/c010batch_challenges/Ch012MaxSumSubarrayK.java
+++ src/main/java/im/mory/java_practice/c010batch_challenges/Ch012MaxSumSubarrayK.java
@@ -2,6 +2,16 @@ package im.mory.java_practice.c010batch_challenges;
 
 public class Ch012MaxSumSubarrayK {
     int findMaxSubSubArray(int k, int[] arr) {
-
+        int left = 0, maxSum = 0, sum = 0;
+        for (int right = 0; right < arr.length; right++) {
+            sum += arr[right];
+            if (right >= k - 1) {
+                if (sum > maxSum)
+                    maxSum = sum;
+                sum -= arr[left];
+                left++;
+            }
+        }
+        return maxSum;
     }
 }
