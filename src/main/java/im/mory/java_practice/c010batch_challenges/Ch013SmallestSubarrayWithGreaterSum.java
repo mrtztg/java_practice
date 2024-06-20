f56b81bc2e6c33c903d01f068cf8fc9848e1f2ec
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-06-20 08:51:48 +0100

    feat: Add Ch013SmallestSubarrayWithGreaterSum logic

--- src/main/java/im/mory/java_practice/c010batch_challenges/Ch013SmallestSubarrayWithGreaterSum.java
+++ src/main/java/im/mory/java_practice/c010batch_challenges/Ch013SmallestSubarrayWithGreaterSum.java
@@ -2,6 +2,18 @@ package im.mory.java_practice.c010batch_challenges;
 
 public class Ch013SmallestSubarrayWithGreaterSum {
     int findMinSubArray(int S, int[] arr) {
-        int left = 0, right = 0;
+        int left = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+        for (int right = 0; right < arr.length; right++) {
+            sum += arr[right];
+            while (sum >= S && left <= right) {
+                int thisLength = right - left + 1;
+                minLength = Math.min(thisLength, minLength);
+                sum-= arr[left];
+                left++;
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0: minLength;
     }
 }
